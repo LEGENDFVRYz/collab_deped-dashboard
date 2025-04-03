2a1d910a59aa8089c5808ceea959e85b03eb7984
Christian-Villanueva <christianaprilkimvillanueva@gmail.com>
2025-04-03 12:50:21 +0800

    added cards in school insights and analytics page

--- src/pages/analytics.py
+++ src/pages/analytics.py
@@ -1,5 +1,5 @@
 import dash
-from dash import html
+from dash import html, dcc
 
 # src/pages/dashboard.py
 from src.components.card import Card
@@ -11,14 +11,49 @@ dash.register_page(__name__, path="/analytics")
 layout = html.Div([
     ## -- Standard: Page Content Header
     html.Div([
-        html.H1('Analytical Tools')
-    ], className='page-header'),
-    
-    ## -- Content Container
-    html.Div([
-            ## -- Insert Here
-
-
-
-    ], className='content')
+        html.Div([
+            html.Div([
+                html.H1('Analytical Tools')
+            ], className='page-header'),
+            
+            html.Div([
+                html.Div([
+                    html.Img(src="/assets/images/icons_navigation/filter.svg")
+                ], className="search-icon"),
+                dcc.Input(
+                    id='input-text',
+                    type='text',
+                    placeholder='Search here . . .',
+                    className="search-input"
+                ),
+                html.Div(["X"], className="clear-button")
+            ], className='search-bar'),
+            
+            html.Div([
+                html.Div(["Sample"], className='gray-1'),
+                html.Div([
+                    html.Img(src="/assets/images/icons_navigation/filter.svg")
+                ], className="filter-icon"),
+            ], className='buttons-level'),
+            
+            html.Div([
+                Card()
+            ], className='card-1'),
+            
+        ], className='left-content'),
+        
+        html.Div([
+            html.Div([
+                html.Div(['Option 1'], className='option-1'),
+                html.Div(['Option 2'], className='option'),
+                html.Div(['Option 3'], className='option'),
+                html.Div(['Option 4'], className='option'),
+            ], className='tab-options'),
+            
+            html.Div([
+                Card()
+            ], className='content-card')
+            
+        ], className='right-content'),
+    ], className='content-section'),
 ], className='analytics-page container')
--- src/pages/school-level.py
+++ src/pages/school-level.py
@@ -11,14 +11,36 @@ dash.register_page(__name__, path="/school-level")
 layout = html.Div([
     ## -- Standard: Page Content Header
     html.Div([
-        html.H1('School-Level Insights:')
+        html.H1('School-Level Insights')
     ], className='page-header'),
     
-    ## -- Content Container
     html.Div([
-            ## -- Insert Here
-
-
-
+        html.Div([
+            html.Span(["Sample"]),
+        ], className='gray-1'),
+        html.Div([
+            html.Span(["EX"]),
+        ], className='gray-2'),
+        html.Div([
+            html.Img(src="/assets/images/icons_navigation/filter.svg")
+        ], className="filter-icon")
+    ], className='buttons-level'),
+    
+    html.Div([
+        html.Div([
+            html.Div([
+                html.Div([Card()], className='card-1'),
+            ], className='si-layer-1'),
+            html.Div([
+                html.Div([Card()], className='card-2'),
+                html.Div([Card()], className='card-3'),
+            ], className='si-layer-2')
+        ], className='main-content'),
+        
+        html.Div([
+            html.Div([
+                html.Div([Card()], className='card-4'),
+            ], className='si-layer-1'),
+        ], className='side-content')
     ], className='content')
 ], className='school-level-page container')
