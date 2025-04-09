b9be9312c789fb48f22e0aaf030e632b2fadb7fb
Christian-Villanueva <christianaprilkimvillanueva@gmail.com>
2025-04-10 04:08:12 +0800

    changed icons

--- src/app.py
+++ src/app.py
@@ -30,7 +30,7 @@ app.layout = html.Div(
                     html.Div([], className='indicator'),
                     dcc.Link([
                             html.Div([html.Img(src="/assets/images/icons_navigation/overview-light.svg")], className='light icon'),
-                            html.Div([html.Img(src="/assets/images/icons_navigation/overview-green.svg")], className='dark icon'),
+                            html.Div([html.Img(src="/assets/images/icons_navigation/overview-blue.svg")], className='dark icon'),
                             html.Div(['Overview'], className='text')
                         ], href="/", className='overview nav-btn')
                 ], id='nav-1', className='item-ctn'),
@@ -38,8 +38,8 @@ app.layout = html.Div(
                 html.Div([
                     html.Div([], className='indicator'),
                     dcc.Link([
-                            html.Div([html.Img(src="/assets/images/icons_navigation/pie-chart-fill.svg")], className='light icon'),
-                            html.Div([html.Img(src="/assets/images/icons_navigation/pie-chart-2-fill.svg")], className='dark icon'),
+                            html.Div([html.Img(src="/assets/images/icons_navigation/analytics-white.svg")], className='light icon'),
+                            html.Div([html.Img(src="/assets/images/icons_navigation/analytics-blue.svg")], className='dark icon'),
                             html.Div(['Analytical Tools'], className='text')
                         ], href='/analytics', className='overview nav-btn'),
                 ], id='nav-2', className='item-ctn'),
@@ -47,8 +47,8 @@ app.layout = html.Div(
                 html.Div([
                     html.Div([], className='indicator'),
                     dcc.Link([
-                            html.Div([html.Img(src="/assets/images/icons_navigation/pie-chart-fill.svg")], className='light icon'),
-                            html.Div([html.Img(src="/assets/images/icons_navigation/pie-chart-2-fill.svg")], className='dark icon'),
+                            html.Div([html.Img(src="/assets/images/icons_navigation/school-level-1-white.svg")], className='light icon'),
+                            html.Div([html.Img(src="/assets/images/icons_navigation/school-level-1-blue.svg")], className='dark icon'),
                             html.Div(['School-level'], className='text')
                         ], href='/school-level', className='overview nav-btn'),
                 ], id='nav-3', className='item-ctn'),
@@ -59,26 +59,26 @@ app.layout = html.Div(
                 html.Div([
                     html.Div([], className='indicator'),
                     dcc.Link([
-                            html.Div([html.Img(src="/assets/images/icons_navigation/pie-chart-fill.svg")], className='light icon'),
-                            html.Div([html.Img(src="/assets/images/icons_navigation/pie-chart-2-fill.svg")], className='dark icon'),
+                            html.Div([html.Img(src="/assets/images/icons_navigation/updates-white.svg")], className='light icon'),
+                            html.Div([html.Img(src="/assets/images/icons_navigation/updates-blue.svg")], className='dark icon'),
                             html.Div(['Updates'], className='text')
                         ], href="/updates", className='overview nav-btn')
                 ], id='opt-1', className='item-ctn'),
                 
                 html.Div([
-                    html.Div([], className='indicator'),
-                    dcc.Link([
-                            html.Div([html.Img(src="/assets/images/icons_navigation/pie-chart-fill.svg")], className='light icon'),
-                            html.Div([html.Img(src="/assets/images/icons_navigation/pie-chart-2-fill.svg")], className='dark icon'),
-                            html.Div(['Help'], className='text')
-                        ], href='/help', className='overview nav-btn'),
+                    # html.Div([], className='indicator'),
+                    # dcc.Link([
+                    #         html.Div([html.Img(src="/assets/images/icons_navigation/pie-chart-fill.svg")], className='light icon'),
+                    #         html.Div([html.Img(src="/assets/images/icons_navigation/pie-chart-2-fill.svg")], className='dark icon'),
+                    #         html.Div(['Help'], className='text')
+                    #     ], href='/help', className='overview nav-btn'),
                 ], id='opt-2', className='item-ctn'),
                 
                 html.Div([
                     html.Div([], className='indicator'),
                     dcc.Link([
-                            html.Div([html.Img(src="/assets/images/icons_navigation/pie-chart-fill.svg")], className='light icon'),
-                            html.Div([html.Img(src="/assets/images/icons_navigation/pie-chart-2-fill.svg")], className='dark icon'),
+                            html.Div([html.Img(src="/assets/images/icons_navigation/settings-1-fill.svg")], className='light icon'),
+                            html.Div([html.Img(src="/assets/images/icons_navigation/settings-1.svg")], className='dark icon'),
                             html.Div(['Settings'], className='text')
                         ], href='/settings', className='overview nav-btn'),
                 ], id='opt-3', className='item-ctn'),
