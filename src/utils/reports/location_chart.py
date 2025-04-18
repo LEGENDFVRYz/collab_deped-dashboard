f21c29df9ee52ce8889547143e8b43a2ba24eba7
LEGENDFVRYz <cruzsebastienalt28@gmail.com>
2025-04-18 11:15:48 +0800

    added: sample charts layout template

---
+++ src/utils/reports/location_chart.py
@@ -0,0 +1,107 @@
+import re, os, sys
+import dash
+from dash import dcc, html
+
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+from config import project_root
+from utils.get_data import auto_extract
+
+
+"""
+    Analytics/Location Charts and Indicator
+    
+"""
+
+
+#################################################################################
+##                     MAIN DATAFRAME BASED FROM THE QUERY                     ##
+#################################################################################
+
+# ## -- This only a temporary dataframe for testing your charts, you can change it
+FILTERED_DF = dataframe = auto_extract(['counts'], is_specific=False)
+FILTERED_DF
+
+# ## -- Check the document for all valid columns and structurette
+# ## -- Dont change the all caps variables
+#################################################################################
+
+
+
+
+#################################################################################
+#################################################################################
+## -- EXAMPLE CHHART
+
+# Manipulated Data for charts
+query = FILTERED_DF[['grade']]
+
+query['school-level'] = query['grade'].apply(
+    lambda x: 'JHS' if x in ['G7', 'G8', 'G9', 'G10', 'JHS'] else ('SHS' if x in ['G11', 'G12'] else 'ELEM')
+)
+
+query = query.groupby(['school-level', 'grade']).size().reset_index(name='school_count')
+
+# Ploting
+sample_chart = px.bar(
+    query,
+    x="school_count", 
+    y="grade",
+    color='school-level',
+    color_discrete_map={
+        'ELEM': '#FF899A', 
+        'JHS': '#E11C38', 
+        'SHS': '#930F22'
+    }
+)
+sample_chart
+
+
+
+#################################################################################
+#################################################################################
+
+
+## -- FIND YOUR CHARTS HERE:
+
+################################################################################
+##  --- Distribution of enrollees per location 
+#################################################################################
+# chart_x = px.bar(filtered_df, )
+
+
+
+
+
+#################################################################################
+
+
+
+
+#################################################################################
+##  --- enrollment density (students per location)
+#################################################################################
+# enrollment_density_chart = []
+
+
+
+
+
+#################################################################################
+
+
+
+#################################################################################
+##  --- enrollment density (students per location)
+#################################################################################
+# enrollment_density_chart = []
+
+
+
+
+
+#################################################################################
