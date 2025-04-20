0753d211bd82da69f6960545965b1b527a9b1117
LEGENDFVRYz <cruzsebastienalt28@gmail.com>
2025-04-21 03:31:22 +0800

    added: function for smart extracting filter dataframe

--- src/data.py
+++ src/data.py
@@ -18,28 +18,113 @@ enrollment_db_engine = create_engine(f"sqlite:///{project_root / 'database/proce
 def smart_filter(filter_info={}) -> pd.DataFrame:
     try:
         # Base query (JOIN: enrollment and sch_info)
-        query = "SELECT * FROM enrollment LEFT JOIN sch_info USING(beis_id) WHERE 1=1"  
+        first_query = "SELECT * FROM enrollment LEFT JOIN sch_info USING(beis_id) WHERE 1=1"  
+        params = {}
         
-        # Check if gender exist in the query
+        first_scope = {
+                'sector' : lambda x: f":sector_{x}",
+                'sub_class' :lambda x: f":subclass_{x}",
+                'type' :lambda x: f":type_{x}",
+                'mod_coc' :lambda x: f":mod_coc_{x}"
+        }
+        
+        second_scope = {
+                'region' : lambda x: f":region_{x}",
+                'province' :lambda x: f":province_{x}",
+                'division' :lambda x: f":division_{x}",
+                'district' :lambda x: f":district_{x}",
+                'minicipality' :lambda x: f":municipality_{x}",
+                'brgy' :lambda x: f":district_{x}"
+        }
+        
+        third_scope = {
+                'mod_coc' : lambda x: f":mod_coc_{x}",
+                'grade' :lambda x: f":grade_{x}",
+                'track' :lambda x: f":track_{x}",
+                'strand' :lambda x: f":strand_{x}",
+                'year' :lambda x: f":year_{x}",
+        }
+        
+        
+        # Check if there is column exist in the first_scope
         if any(key in filter_info for key in ['gender', 'sector', 'sub_class', 'type', 'mod_coc']):
             for col_key, value in filter_info.items():
                 
                 if col_key in ['sector', 'sub_class', 'type', 'mod_coc']:
                     ## Multiples
-                    placeholders = ', '.join([f":region_{i}" for i in range(len(regions))])
-                    params = {f"region_{i}": region for i, region in enumerate(regions)}
-                    
-                    
-                elif col_key in ['gender']:
+                    placeholders = ', '.join([first_scope[col_key](i) for i in range(len(value))])
+                    params = {**params, **{first_scope[col_key](i)[1:]: v for i, v in enumerate(value)}}
+
+                    first_query += f"\nAND {col_key} IN ({placeholders})"
+                        
+                if col_key in ['gender']:
                     ## Single
-                    query += f"\nAND gender == '{filter_info['gender']}'"
-            
-        # JOIN: sch_info table manipulation
-        # query = 
+                    first_query += f"\nAND gender == '{filter_info['gender']}'"
         
-        # dataframe = pd.read_sql_query(main_query, con=engine)
-        # return dataframe
-            print(query)
+
+        # JOIN: first_query + location detailes
+        if any(key in filter_info for key in ['region', 'province', 'division', 'district']):
+            region_query = f"""SELECT * FROM (
+                {first_query}
+            ) LEFT JOIN sch_region USING (region_id)
+            LEFT JOIN sch_local USING (local_id)
+            WHERE 1=1
+            """
+            for col_key, value in filter_info.items():
+                if col_key in ['region', 'province', 'division', 'district', 'municipality', 'brgy']:
+                    ## LOCATION
+                    placeholders = ', '.join([second_scope[col_key](i) for i in range(len(value))])
+                    params = {**params, **{second_scope[col_key](i)[1:]: v for i, v in enumerate(value)}}
+                    
+                    region_query += f"\nAND {col_key} IN ({placeholders})"
+        else:
+            # NO LOCATION FILTER SELECTED
+            region_query = first_query
+        
+        
+        ## JOIN: region_query + grade
+        if any(key in filter_info for key in ['mod_coc', 'grade', 'strand', 'track', 'year']):
+            grade_query = """SELECT * FROM (
+                SELECT enroll_id, beis_id, gender, '__NaN__' as track, '__NaN__' as strand, grade, counts, year FROM enrollment
+                INNER JOIN ES_enroll USING(enroll_id)
+
+                UNION ALL
+
+                SELECT enroll_id, beis_id, gender, '__NaN__' as track, '__NaN__' as strand, grade, counts, year FROM enrollment 
+                INNER JOIN JHS_enroll USING(enroll_id)
+
+                UNION ALL
+
+                SELECT enroll_id, beis_id, gender, track, strand, grade, counts, year FROM enrollment 
+                INNER JOIN SHS_enroll USING(enroll_id)
+            )
+            WHERE 1=1"""
+            
+            for col_key, value in filter_info.items():
+                if col_key in ['mod_coc', 'grade', 'strand', 'track', 'year']:
+                    ## LOCATION
+                    placeholders = ', '.join([third_scope[col_key](i) for i in range(len(value))])
+                    params = {**params, **{third_scope[col_key](i)[1:]: v for i, v in enumerate(value)}}
+
+                    grade_query += f"\nAND {col_key} IN ({placeholders})"
+            
+            ## MAIN QUERY
+            main_query = f"""
+                SELECT * FROM (
+                    {region_query}
+                ) INNER JOIN (
+                    {grade_query}
+                ) USING (enroll_id, beis_id, gender)
+            """
+        else:
+            main_query = region_query
+            
+            
+        dataframe = pd.read_sql_query(main_query, con=enrollment_db_engine, params=params)
+        return dataframe
+        
+        # print(grade_query)
+        # print("params:\n", params)
         
     except Exception as e:
         print(f'error: {e}')
@@ -48,43 +133,38 @@ def smart_filter(filter_info={}) -> pd.DataFrame:
 def get_engine():
     return enrollment_db_engine
     
-if __name__ == '__main__':
     
+    
+if __name__ == '__main__':
     sample = {
         "sector": [
-            "Public",
-            "Private"
-            ],
-            "gender": "M",
-            "region": [
-                "CAR"
-            ],
-            "province": [
-                "BENGUET"
-            ],
-            "division": [
-                "Baguio City"
-            ],
-            "district": [
-                "District II",
-                "District III"
-            ]
-        }
+            "Private",
+            "Public"
+        ],
+        "subclass": [
+            "Non-Sectarian"
+        ],
+        "track": [
+            "ARTS"
+        ],
+        # "grade": [
+        #     "G8",
+        #     "G9",
+        #     "G10"
+        # ],
+        "region": [
+            "MIMAROPA"
+        ],
+        "province": [
+            "ORIENTAL MINDORO"
+        ],
+        "division": [
+            "Calapan City"
+        ],
+        "district": [
+            "Calapan South"
+        ]
+    }
     
-    # query = smart_filter(sample)
-    
-    
-    regions = ['NCR', 'BARMM', 'PSO']
-
-    # Generate placeholders for the IN clause
-    placeholders = ', '.join([f":region_{i}" for i in range(len(regions))])
-    placeholders
-
-    # Build the SQL query
-    query = f"SELECT * FROM sch_region WHERE region IN ({placeholders})"
-    params = {f"region_{i}": region for i, region in enumerate(regions)}
-    
-    # Read the SQL query
-    df = pd.read_sql(query, con=enrollment_db_engine, params=params)
-    
-    print(query)
+    df = smart_filter(sample)
+    df
