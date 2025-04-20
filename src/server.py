9093c0ec0bb94eba9a5b9d0fa878b178b7c1ba14
LEGENDFVRYz <cruzsebastienalt28@gmail.com>
2025-04-20 16:26:28 +0800

    modified: add server cache

--- src/server.py
+++ src/server.py
@@ -1,3 +1,19 @@
 from flask import Flask
+from flask_caching import Cache
+from src.utils.get_data import auto_extract
 
-server = Flask(__name__)  # Create Flask server
+# Create Flask server
+server = Flask(__name__)  
+
+
+# Create Cache
+cache = Cache(config={'CACHE_TYPE': 'simple'})
+
+# Load and cache full dataset (on server)
+@cache.memoize()
+def load_location_data():
+    # Fetch the data using SQL query
+    dataframe = auto_extract([
+        'region', 'division', 'district', 'province', 'municipality', 'brgy'
+    ], is_specific=True, distinct=True)
+    return dataframe
