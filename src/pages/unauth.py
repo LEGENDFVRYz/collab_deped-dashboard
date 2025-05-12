bbd8e57573e14fdb4c0525260366eb35182ba341
LEGENDFVRYz <cruzsebastienalt28@gmail.com>
2025-05-12 19:11:12 +0800

    added: unauthorized message

--- src/pages/unauth.py
+++ src/pages/unauth.py
@@ -2,14 +2,6 @@ import dash
 from dash import html
 from dash import dcc, html
 
-# --  Shared Components
-from src.components.card import Card
-
-# -- Graphs
-from src.utils.reports import home_chart
-# from src.utils.reports.home_enrollment_per_region import home_regional_distribution, home_enrollment_per_region, home_school_number_per_sector, home_gender_distribution, home_subclass_table, home_program_offering, home_shs_tracks, home_shs_strands
-# from src.utils.reports.home_enrollment_per_region import total_enrollees, number_of_schools, number_of_schools_formatted, total_male_count, total_male_count_formatted, total_female_count, total_female_count_formatted, total_es_count, total_es_count_formatted, total_jhs_count, total_jhs_count_formatted, total_shs_count, total_shs_count_formatted, gender_gap, greater_gender, lesser_gender
-
 
 # Landing page
 dash.register_page(__name__, path="/unauthorized", suppress_callback_exceptions=True)  
